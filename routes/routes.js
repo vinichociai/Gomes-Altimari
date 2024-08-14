445674ca7316e94307fbab312cb0242001ed57be
ViniciusMendonca12 <vivicomferreira@hotmail.com>
2024-08-13 22:38:59 -0300

    subindo

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,19 @@
 var express = require("express");
-var app = express();
 var router = express.Router();
-var DashboardController = require("../controllers/DashboardController")
+var DashboardController = require("../controllers/DashboardController");
+var AnalisePublicacaoController = require("../controllers/AnalisePublicacaoController");
+var ProvidenciasController = require("../controllers/ProvidenciasController");
 
-router.get("/dashboard", DashboardController.carregaPaginaDashboard)
-router.get("/analise-de-divergencia", DashboardController.carregaPaginaAnaliseDivergencia)
-router.get("/advcontrolador", DashboardController.carregaPaginaAdvControlador)
-router.get("/advExecutor", DashboardController.carregaPaginaAdvExecutor)
-router.get("/visualizar-providencias", DashboardController.carregaPaginaVisualizarProvidencias)
+module.exports = function(io) {
+    router.get("/dashboard", (req, res) => DashboardController.carregaPaginaDashboard(req, res, io));
+    router.get("/analise-de-divergencia", (req, res) => DashboardController.carregaPaginaAnaliseDivergencia(req, res, io));
+    router.get("/advcontrolador", (req, res) => DashboardController.carregaPaginaAdvControlador(req, res, io));
+    router.get("/advExecutor", (req, res) => DashboardController.carregaPaginaAdvExecutor(req, res, io));
+    router.get("/visualizar-providencias", (req, res) => DashboardController.carregaPaginaVisualizarProvidencias(req, res, io));
 
+    router.post("/analise-publicacao", (req, res) => AnalisePublicacaoController.analisePublicacao(req, res, io));
 
-module.exports = router;
+    router.post("/cadastraProvidencias", (req, res) => ProvidenciasController.cadastrarProvidencias(req, res, io));
+
+    return router;
+};
