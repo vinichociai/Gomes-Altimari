d0430d2fff11e5439af68295262307a03c46e664
ViniciusMendonca12 <94076366+ViniciusMendonca12@users.noreply.github.com>
2024-10-25 19:12:22 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : 'localhost',
-      user : 'root',
-      password : '1538minha',
-      database : 'gaa'
+      host : '****',
+      user : '****',
+      password : '****',
+      database : '****'
     }
   });
 
